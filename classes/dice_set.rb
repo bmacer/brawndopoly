859309a4f51a9303b10007778c0bbfdba4094bdc
Brandon Macer (bmacer) <bmacer@cisco.com>
2019-01-24 08:07:35 -0500

    add double-dice rolling basic functionality

--- classes/dice_set.rb
+++ classes/dice_set.rb
@@ -4,8 +4,17 @@ class DiceSet < Dice
 
   def initialize
     @die_1, @die_2 = Dice.new, Dice.new
-    puts @die_1
-    puts @die_2
+    display
+  end
+
+  def display
+    "[#{@die_1.value}] [#{@die_2.value}]"
+  end
+
+  def roll
+    @die_1.roll
+    @die_2.roll
+    display
   end
 
 end
